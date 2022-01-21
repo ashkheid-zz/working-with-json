4c1178a1d9f0560880fdbc693a98cabb9d928863
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-01-21 20:59:13 +0330

    Just a draft

---
+++ temp.js
@@ -0,0 +1,52 @@
+let element = 'section[ashkan]';
+
+function selector(str) {
+  let hasElement, hasAttribute, element, attribute, arr, items;
+
+  const validArgRegex = /^([A-Za-z0-9]+|\[)+[A-Za-z0-9]([A-Za-z0-9]+|\])$/g;
+  try {
+    if (!validArgRegex.test(str)) {
+      throw new Error("Please check the element you've passed in!");
+    }
+
+    items = str.split(/[\[\]]/g).filter(Boolean); //triming the array after splitting it
+    console.log(items);
+
+    switch (str.indexOf('[')) {
+      case 0:
+        // 0: it is only an attribute
+        [hasElement, hasAttribute, element, attribute] = [
+          false,
+          true,
+          null,
+          items[0],
+        ];
+        break;
+      case -1:
+        // -1: it is only an element
+        [hasElement, hasAttribute, element, attribute] = [
+          true,
+          false,
+          items[0],
+          null,
+        ];
+        break;
+      default:
+        // any positive number: it is a element with an attribute
+        [hasElement, hasAttribute, element, attribute] = [
+          true,
+          true,
+          items[0],
+          items[1],
+        ];
+        break;
+    }
+
+    console.log(hasElement, hasAttribute, element, attribute);
+  } catch (err) {
+    err.name = 'Invalide Selector Error';
+    console.error(err.stack);
+  }
+}
+
+selector(element);
